de6bf62331196462ee00bf2d05f85ef3377e8ffd
Ali Murad <herralimurad@gmail.com>
2019-04-17 00:58:45 +0500

    Initial Commit

---
+++ MovieApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MovieApp
+//
+//  Created by Ali Murad on 17/04/2019.
+//  Copyright © 2019 Ali Murad. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
